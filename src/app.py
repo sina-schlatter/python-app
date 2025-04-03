5fb9ab96e7d962d7b3dc1645482d559ebc40dbbf
sina-schlatter <sthings@sina-dev.labul.sva.de>
2025-04-03 11:40:51 +0000

    update app.py

--- src/app.py
+++ src/app.py
@@ -6,13 +6,14 @@ import socket
 app = Flask(__name__)
 
 
-@app.route('/api/v1/details')
+@app.route('/api/v1/info')
 
 def details():
     return jsonify({
     	'time': datetime.datetime.now().strftime("%I:%M:%S%p  on %B %d, %Y"),
     	'hostname': socket.gethostname(),
-        'message': 'You are doing great, human! <3 !'
+        'message': 'You are doing great, human! <3 !',
+        'deployed_on': 'kubernetes'
     })
 
 @app.route('/api/v1/healthz')
